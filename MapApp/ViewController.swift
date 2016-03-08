a65639748bd9f90cb3128b489ba08a443ee9cddf
Abhijeet Chaudhary <abhijeetchz@Abhijeets-MacBook-Pro.local>
2016-03-08 13:34:10 +0100

    added files for MapApp

--- MapApp/ViewController.swift
+++ MapApp/ViewController.swift
@@ -7,12 +7,23 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+    
+    
+
+    @IBOutlet weak var MapView: MKMapView!
+    @IBOutlet weak var btnLocation: UIButton!
+    
+    
+    let locationMgr = CLLocationManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+       locationMgr.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +31,33 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func btnLocationACT(sender: UIButton) {
+        
+        locationMgr.requestWhenInUseAuthorization()
+        locationMgr.desiredAccuracy = kCLLocationAccuracyBest
+        locationMgr.startUpdatingLocation()
+        
+    }
 
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let location = locations.last
+        let center = CLLocationCoordinate2DMake(location!.coordinate.latitude, location!.coordinate.longitude)
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let region = MKCoordinateRegion(center: center, span: span)
+        
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = center
+        
+        MapView.addAnnotation(annotation)
+        MapView.setRegion(region, animated: true)
+        locationMgr.stopUpdatingLocation()
+        
+    }
+    
+    
+    
+    
 }
 
